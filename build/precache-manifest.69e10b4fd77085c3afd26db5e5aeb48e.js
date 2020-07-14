ac5439af3a2bdbf1fcca25b597478d43f75d7977
sylvain joly <sylvain.joly00@orange.fr>
2020-07-14 16:55:48 +0200

    [UPDATE] cannot build on AWS

---
+++ build/precache-manifest.69e10b4fd77085c3afd26db5e5aeb48e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51f99dd5e9cc9ec1247ea2ce73ae907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82694eb2c8469d4c9897",
+    "url": "/static/css/2.f766059f.chunk.css"
+  },
+  {
+    "revision": "35e0aad48d0a561fc9b5",
+    "url": "/static/css/main.09dcdbfa.chunk.css"
+  },
+  {
+    "revision": "82694eb2c8469d4c9897",
+    "url": "/static/js/2.94ec23e1.chunk.js"
+  },
+  {
+    "revision": "abc48cf9a51c21b35817d42d2a8a72fe",
+    "url": "/static/js/2.94ec23e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e0aad48d0a561fc9b5",
+    "url": "/static/js/main.9ae88b2b.chunk.js"
+  },
+  {
+    "revision": "edfb7aff01f1ec5f7a2b",
+    "url": "/static/js/runtime-main.93bc1476.js"
+  },
+  {
+    "revision": "d90d21ddd9d8925df12fa0ce2a59f31e",
+    "url": "/static/media/JOLY_Sylvain.d90d21dd.png"
+  },
+  {
+    "revision": "baa251f36c8bee6ee1533d281036cb0f",
+    "url": "/static/media/MALLET_Thomas.baa251f3.png"
+  },
+  {
+    "revision": "b1b28fafbbb7e936435116d3a9532931",
+    "url": "/static/media/cat-photography.b1b28faf.png"
+  },
+  {
+    "revision": "ff41ff844e593d1eee1679821ba94fa6",
+    "url": "/static/media/dog.ff41ff84.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
